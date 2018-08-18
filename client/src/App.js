f703da6566396fb6e416fe9386237318d7940e9c
Luke Camelo <azathothbeats@gmail.com>
2018-08-18 17:27:26 -0400

    Added server directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  text-align: center;
+`
+const Header = styled.div`
+  background-color: papayawhip;
+  height: 150px;
+  padding: 20px;
+  color: palevioletred;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Wrapper>
+        <Header>
+         <h1>PERN-Forum prototype</h1>
+        </Header>
+      </Wrapper>
+    )
   }
 }
 
-export default App;
+export default App
