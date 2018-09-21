233c61d877ff5e0a581f88aac3defaede8b3ab05
Luke Camelo <azathothbeats@gmail.com>
2018-09-21 02:56:38 -0400

    Added tests for threadReducer

--- client/src/tests/reducers/threadReducer.test.js
+++ client/src/tests/reducers/threadReducer.test.js
@@ -1,11 +1,41 @@
-import React from 'react'
+// import React from 'react'
 import reducer from '../../reducers/threadReducer'
 import * as types from '../../actions/types'
 
-describe('thread reducer', () => {
+const initialState = {
+  users: [],
+  threads: [],
+  posts: []
+}
 
+describe('thread reducer', () => {
   it('should return the initial state', () => {
-    
+    expect(reducer(undefined, {})).toEqual({
+      users: [],
+      threads: [],
+      posts: []
+    })
   })
 
+  it('should handle FETCH_POSTS', () => {
+    const fetchAction = {
+      type: types.FETCH_POSTS,
+      payload: [
+        {
+          author: 'rediscover',
+          content: 'derp',
+          userId: 1,
+          id: 1,
+          threadId: 1
+        }
+      ]
+    }
+  
+    expect(reducer(initialState, fetchAction)).toEqual({
+      ...initialState,
+      posts: [
+        { author: 'rediscover', content: 'derp', userId: 1, id: 1, threadId: 1 }
+      ]
+    })
+  })
 })
