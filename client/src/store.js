446ae8d850e75f3a095a3e8f3b971382dcc9b0ff
Luke Camelo <azathothbeats@gmail.com>
2018-09-26 19:50:37 -0400

    Deploying to Heroku

--- client/src/store.js
+++ client/src/store.js
@@ -13,7 +13,7 @@ const store = createStore(
   compose(
     applyMiddleware(...middleware),
     // Activates redux devtools
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
