b0a0fb3a42d4a990e637bd1d32866d40da723deb
Luke Camelo <azathothbeats@gmail.com>
2018-09-26 20:23:41 -0400

    Changed the way express serves static files

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ const auth = require('./server/routes/auth')
 const index = require('./server/routes/routes')
 const thread = require('./server/routes/thread')
 
-app.use(express.static(path.join(__dirname, 'client/public')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use('/', index)
 app.use('/thread', thread)
 app.use('/user', passport.authenticate('jwt', { session: false }), user)
 app.use('/auth', auth)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 // models.sequelize.sync({ force: true }).then(() => {
