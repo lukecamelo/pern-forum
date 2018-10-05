231ee9205f5c913a0ee84f7d4c53409a5e4d52b1
Luke Camelo <azathothbeats@gmail.com>
2018-10-05 19:58:22 -0400

    Reorganized Post.Controls styling

--- client/src/styled/Post/Controls.js
+++ client/src/styled/Post/Controls.js
@@ -1,11 +1,23 @@
 import styled from 'styled-components'
 
-const Controls = styled.div`
-  text-align: right;
+
+const Controls = styled.div` 
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
   padding-bottom: 1em;
-    @media screen and (max-width: 532px) {
-      padding: 0 1em .5em 1em;
-    }
+
+  > p {
+    margin-right: 1em;
+    color: #0266c8;
+    margin-bottom: 0;
+  }
+  
+  @media screen and (max-width: 532px) {
+    padding: 0 1em .5em 1em;
+    justify-content: space-between;
+  }
 `
 
 export default Controls
