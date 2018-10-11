2eedbc22b912766e4d77480c5eba86f6215828c7
Luke Camelo <azathothbeats@gmail.com>
2018-10-10 23:56:43 -0400

    Added even fancier loader

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
+import './Loader.css'
 
 const rotation = keyframes`
   100% { transform: rotate(360deg) }
@@ -12,13 +13,14 @@ const Spinning = styled.div`
   right: 0;
   margin: auto;
   animation: 2s ${rotation};
-  color: ${props => props.theme.primary}
+  color: ${props => props.theme.primary};
 `
 
 const Loader = () => {
   return (
     <div>
-      <Spinning className="fas fa-spinner fa-7x" />
+      {/* <Spinning className="fas fa-spinner fa-7x" /> */}
+      <div className="loader">Loading...</div>
     </div>
   )
 }
