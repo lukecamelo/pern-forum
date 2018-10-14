97ee46a6d33e9c9c9af63894514d6d7c5f034c76
Luke Camelo <azathothbeats@gmail.com>
2018-10-13 20:12:12 -0400

    Spiced up the landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from './UserControlPanel'
-import { NavBar } from './NavBar'
+import NavBar from './NavBar'
 import styled from 'styled-components'
 
 const Header = styled.h1`
@@ -11,6 +11,9 @@ const Header = styled.h1`
   font-family: 'Roboto', sans-serif;
   font-weight: 600;
   > strong {
+    @media screen and (max-width: 532px) {
+      display: block;
+    }
     color: #0266c8;
   }
 `
@@ -20,6 +23,14 @@ const Li = styled.li`
   padding: 1em 0;
   display: flex;
   justify-content: flex-start;
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+    align-items: center;
+    > p {
+      text-align: center;
+      width: 300px;
+    }
+  }
 `
 const Ul = styled.ul`
   margin: 0;
@@ -29,41 +40,48 @@ const Ul = styled.ul`
 const I = styled.i`
   min-height: 50px;
   min-width: 50px;
-  margin-right: .5em;
+  margin-right: 0.5em;
   color: #0266c8;
 `
 const P = styled.p`
+  font-size: 18px;
+  line-height: 1.5;
   margin: 0;
   display: inline-block;
   text-align: left;
+`
+const Strong = styled.strong`
+  color: #0266c8;
 `
 const Landing = () => {
   return (
     <React.Fragment>
       <NavBar />
       <Card style={{ color: '#3d4852' }}>
-        <Header>Welcome to the <strong>imp zone</strong>.</Header>
+        <Header>
+          Welcome to the <strong>imp zone</strong>
+        </Header>
         <Ul>
           <Li>
             <I className="fas fa-gamepad fa-2x" />
             <P>
-              Greatest video game forum on the internet, now stand alone and not
-              based on 20 year old software
+              Greatest video game forum on the internet, now{' '}
+              <Strong>stand alone</Strong> and not based on 20 year old software
             </P>
           </Li>
           <Li>
             <I className="fas fa-ban fa-2x" />
             <P>
-              Completely lowtax free, and therefore will recieve timely updates
-              and a modern design
+              Completely <Strong>lowtax free</Strong>, and therefore will
+              recieve timely updates and a modern design
             </P>
           </Li>
           <Li>
             <I className="fas fa-users fa-2x" />
             <P>
-              Also it's regular free, gone are the days of paying 10bux for the
-              privilege to post on, as mentioned before, the greatest video game
-              forum on the internet
+              Also it's <Strong>regular free</Strong>, gone are the days of
+              paying 10bux for the privilege to post on, as mentioned before,
+              the greatest video game forum on the internet
             </P>
           </Li>
         </Ul>
