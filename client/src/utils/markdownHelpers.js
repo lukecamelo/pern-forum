fc75131adc0948d5545ab4571fec31e48dbd1e60
Luke Camelo <azathothbeats@gmail.com>
2018-10-14 21:24:58 -0400

    Nested quotes are now removed for readability

--- client/src/utils/markdownHelpers.js
+++ client/src/utils/markdownHelpers.js
@@ -2,12 +2,18 @@ export const quotePostInEditor = (quotedUser, quotedPost) => {
   return (
     '<blockquote>\n' +
     quotedUser +
-    ' said \n' +
-    quotedPost +
+    ' said' +
+    removeNestedQuotes(quotedPost) +
     '\n</blockquote>'
   )
 }
 
 export const clearEditor = () => {
   return ''
+}
+
+const removeNestedQuotes = string => {
+  let regex = /<blockquote>[\s\S]*<\/blockquote>/
+  let new_str = string.replace(regex, '');
+  return new_str
 }
