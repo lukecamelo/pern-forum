3daa77ac36e8410322481493b348d4fd5278e39e
Luke Camelo <azathothbeats@gmail.com>
2018-10-24 23:02:47 -0400

    More subforum foundation laying

---
+++ client/src/containers/Subforum.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ThreadList from './ThreadList'
+import Pagination from '../components/Pagination'
+
+class Subforum extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>
+          put what has so far been living in App.js here instead, corresponding
+          to the subforum selected
+        </h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Subforum
