2d5bdf88701f426afbff37922aa0d7a237ff9563
Luke Camelo <azathothbeats@gmail.com>
2019-01-13 16:54:33 -0500

    Reworked SubforumList as functional hook based component

--- client/src/containers/SubforumList.js
+++ client/src/containers/SubforumList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import api from '../services/api'
 import moment from 'moment'
@@ -10,92 +10,81 @@ import StyledSubList from '../styled/StyledSubList'
 import LatestThread from '../styled/StyledSubList/LatestThread'
 import Loader from '../components/Loader'
 
-/*
- ************* 
- This whole component is a big mess
- TODO: fix the mess
- ************* 
-*/
+const SubforumList = () => {
+  const [latestGeneral, setLatestGeneral] = useState('')
+  const [latestGames, setLatestGames] = useState('')
+  const [hasLoaded, setHasLoaded] = useState(false)
 
-class SubforumList extends Component {
-  state = {
-    latestGeneral: '...',
-    latestGames: '...',
-    hasLoaded: false
+  async function useLatestThreads() {
+    let general = await api.threads.getLatestSubforumThread(1)
+    let games = await api.threads.getLatestSubforumThread(2)
+    setLatestGeneral(general)
+    setLatestGames(games)
+    setHasLoaded(true)
   }
 
-  componentDidMount = async () => {
-    let latestGeneral = await api.threads.getLatestSubforumThread(1)
-    let latestGames = await api.threads.getLatestSubforumThread(2)
-    this.setState({
-      latestGeneral,
-      latestGames,
-      hasLoaded: true
-    })
-  }
+  useEffect(() => {
+    useLatestThreads()
+  }, [])
 
-  render() {
-    const { hasLoaded, latestGeneral, latestGames } = this.state
-    if (hasLoaded) {
-      return (
-        <React.Fragment>
-          <Banner>
-            <H1 style={{ margin: '0 auto', color: 'white' }}>Forums</H1>
-          </Banner>
-          <StyledSubList>
-            <StyledSubList.SubContainer>
-              <StyledSubList.LinkContainer>
-                <StyledSubList.SubLink to="/subforum/1/page/1">
-                  General Discussion
-                </StyledSubList.SubLink>
-              </StyledSubList.LinkContainer>
+  // const { hasLoaded, latestGeneral, latestGames } = this.state
+  if (hasLoaded) {
+    return (
+      <React.Fragment>
+        <Banner>
+          <H1 style={{ margin: '0 auto', color: 'white' }}>Forums</H1>
+        </Banner>
+        <StyledSubList>
+          <StyledSubList.SubContainer>
+            <StyledSubList.LinkContainer>
+              <StyledSubList.SubLink to="/subforum/1/page/1">
+                General Discussion
+              </StyledSubList.SubLink>
+            </StyledSubList.LinkContainer>
 
-              <LatestThread style={{ textAlign: 'left' }}>
-                <LatestThread.LatestLink
-                  to={`/thread/${this.state.latestGeneral.id}/page/1`}
-                >
-                  {latestGeneral.title}
-                </LatestThread.LatestLink>
+            <LatestThread style={{ textAlign: 'left' }}>
+              <LatestThread.LatestLink
+                to={`/thread/${latestGeneral.id}/page/1`}
+              >
+                {latestGeneral.title}
+              </LatestThread.LatestLink>
 
-                <LatestThread.InfoText>
-                  By {latestGeneral.Post[0].author}
-                </LatestThread.InfoText>
-                <LatestThread.InfoText>
-                  {moment(latestGeneral.Post[0].createdAt).fromNow()}
-                </LatestThread.InfoText>
-              </LatestThread>
-            </StyledSubList.SubContainer>
+              <LatestThread.InfoText>
+                By {latestGeneral.Post[0].author}
+              </LatestThread.InfoText>
+              <LatestThread.InfoText>
+                {moment(latestGeneral.Post[0].createdAt).fromNow()}
+              </LatestThread.InfoText>
+            </LatestThread>
+          </StyledSubList.SubContainer>
 
-            <StyledSubList.SubContainer>
-              <StyledSubList.LinkContainer>
-                <StyledSubList.SubLink to="/subforum/2/page/1">
-                  Video Games
-                </StyledSubList.SubLink>
-              </StyledSubList.LinkContainer>
-              <LatestThread>
-                <LatestThread.LatestLink
-                  to={`/thread/${this.state.latestGames.id}/page/1`}
-                >
-                  {latestGames.title}
-                </LatestThread.LatestLink>
-                <LatestThread.InfoText>
-                  By {latestGames.Post[0].author}
-                </LatestThread.InfoText>
-                <LatestThread.InfoText>
-                  {moment(latestGames.Post[0].createdAt).fromNow()}
-                </LatestThread.InfoText>
-              </LatestThread>
-            </StyledSubList.SubContainer>
-          </StyledSubList>
-        </React.Fragment>
-      )
-    } else {
-      return (
-        <React.Fragment>
-          <Loader />
-        </React.Fragment>
-      )
-    }
+          <StyledSubList.SubContainer>
+            <StyledSubList.LinkContainer>
+              <StyledSubList.SubLink to="/subforum/2/page/1">
+                Video Games
+              </StyledSubList.SubLink>
+            </StyledSubList.LinkContainer>
+            <LatestThread>
+              <LatestThread.LatestLink to={`/thread/${latestGames.id}/page/1`}>
+                {latestGames.title}
+              </LatestThread.LatestLink>
+              <LatestThread.InfoText>
+                By {latestGames.Post[0].author}
+              </LatestThread.InfoText>
+              <LatestThread.InfoText>
+                {moment(latestGames.Post[0].createdAt).fromNow()}
+              </LatestThread.InfoText>
+            </LatestThread>
+          </StyledSubList.SubContainer>
+        </StyledSubList>
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <Loader />
+      </React.Fragment>
+    )
   }
 }
 
